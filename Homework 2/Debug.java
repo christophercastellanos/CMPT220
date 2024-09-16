c7ef380e8e9ebbc6e3311e1a5977a0bb2ddacd9a
christophercastellanos <christopher.castellanos1@marist.edu>
2024-09-15 21:59:24 -0400

    HW2

--- Homework 2/Debug.java
+++ Homework 2/Debug.java
@@ -1,44 +1,29 @@
 
-public class Main {
+public class Debug {
     public static void main(String[] args) {
 
         //for this section: are these all printing the best option? If they aren't, fix it!
         //(However you interpret 'fix' is fine i promise, any way you fix it shows you get the concept to me)
         int var1 = 4;
-        if (var1 > 4){
-            System.out.println("Var1 is greater than 4");
-        }else{
-            System.out.println("Var1 is less than 4");
-        }
-
         int var2 = 6;
-        if (var2 == 5){
-            System.out.println("Var2 is 5");
-        } else if (var2 > 5){
-            System.out.println("Var2 is greater than 5");
-        } else if (var2 < 5){
-            System.out.println("Var2 is less than 5");
-        } else{
-            System.out.println("Var2 is 6");
-        }
-
         int var3 = 5;
-        if (var3 > 10){
-            System.out.println("Var3 is greater than 10");
-        } else {
-            System.out.println("Var3 is less than 10");
+        if (var1 == 4 && var2==6 && var3==5){
+            System.out.println("Var1 is 4, Var2 is 6, and Var3 is 5");
+
+        }else{
+            System.out.println("Variables aren't correct");
         }
-
-
+        // felt like all of them were trying to get to come to the conclusion that the variables are the variables that were already assigned
+        // the variables were provided and with common sense you can see which is larger or smaller
 
 
         //for this section: why are we not entering the if statement?
-        if ("Marist" == "marist"){
+        if ("Marist" == "Marist"){
             System.out.println("Marist college!");
         } else{
             System.out.println("Not marist college :(!");
         }
-
+// capitalization on the "marist", it should be "Marist"
 
     }
 }
--- Homework 2/ifelsepractice.java
+++ Homework 2/ifelsepractice.java
@@ -8,12 +8,43 @@ import java.util.Scanner;
 * can be any topic you pick :) feel free to pick some obscure or niche topics!
 * good luck!
 * */
-public class Main {
+public class ifelsepractice{
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int score=0;
+        System.out.println("What's my favorite color? Purple, Red, or Blue?");
+        String color=sc.nextLine();
 
+        if (color.equals("Purple")||color.equals("purple")){
+            System.out.println("You're right, 2 more to go!");
+            score++;
 
+        }else{
+            System.out.println("Hmm that's not right. NO POINTS!");
+        }
 
+        System.out.println("Who's the best superhero? Batman, Superman, or Spider-man?");
+        String superhero=sc.nextLine();
+
+        if (color.equals("Purple")&&superhero.equals("Batman")|| superhero.equals("batman")){
+            System.out.println("And why is it Batman! 1 more!!!");
+            score++;
+        }else{
+            System.out.println("I love them too but that's not right. . . IDC if it's biased!");
+            
+        }
+        System.out.println("In Pokemon, what is the only pokemon that has no weaknesses? Is it Eelektross, Shedinja, or Malamar? ");
+        String pokemon=sc.nextLine();
+        if(pokemon.equals("Eelektross")||pokemon.equals("eelektross")){
+            System.out.println("You're a Pokemon Master! The whole evolution line has no weakness because they have the ability levitate, making them immune from Ground type moves.");
+            score++;
+
+        }else{
+            System.out.println("Incorrect. The entire Eelektross evolution line has no weakness because they have the ability levitate, making them immune from Ground type moves. Ash would be disappointed. ");
+        }
+        System.out.println("Your total score was "+score+". Thank you for playing have a great day!");
+        }
     }
-}
+// used chat gpt to figure out how to keep track of score.
 
 
